90a8fb13cf78a1e9353fdc4e0785d06e0d26b7ca
Evandro Costa <evandro.souzac@gmail.com>
2024-11-22 14:13:18 -0300

    create `fetchGoogleMaps` file

---
+++ backend/src/lib/fetchGoogleMaps.ts
@@ -0,0 +1,24 @@
+export async function fetchDirections(
+  origin: string,
+  destination: string,
+): Promise<google.maps.DirectionsResult> {
+  try {
+    const originUri = encodeURIComponent(origin);
+    const destinationUri = encodeURIComponent(destination);
+
+    const response = await fetch(
+      `https://maps.googleapis.com/maps/api/directions/json?destination=${destinationUri}&origin=${originUri}&key=${process.env.GOOGLE_API_KEY}`,
+    );
+
+    if (!response.ok) {
+      throw new Error("Error while fetching data.");
+    }
+
+    const data = (await response.json()) as google.maps.DirectionsResult;
+
+    return data;
+  } catch (error) {
+    console.error("Error fetching directions", error);
+    throw error;
+  }
+}
